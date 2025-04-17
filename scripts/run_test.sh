21af867611b8052a94213315ab8be8cabb936b3c
ParkSoobin1219 <113415274+ParkSoobin1219@users.noreply.github.com>
2025-04-17 17:09:49 +0900

    Update run_test.sh

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=3
-
 python test.py \
  --dataroot ./datasets/horse2zebra \
- --results_dir ./results/horse2zebra/db/295 \
- --name db \
+ --results_dir ./results/h2z \
+ --name h2z \
  --batch_size_dec 30 \
  --use_sa_layers True \
  --sa_blocks 3,5 \
@@ -14,4 +10,4 @@ python test.py \
  --netG resnet_9blocks \
  --dataset_mode unaligned \
  --freq_separation True \
- --num_test 550
+ --num_test 120
